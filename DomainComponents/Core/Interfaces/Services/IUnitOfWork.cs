c52098247ea2c291512c523eb7013ed52de6ae39
Nikita <nikitaklime2005@gmail.com>
2025-10-16 01:01:47 +0300

    ADDED: Core changes, added Interfaces for Unit Of Work pattern

---
+++ DomainComponents/Core/Interfaces/Services/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+using DomainComponents.Core.Models;
+
+namespace DomainComponents.Core.Interfaces.Services
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        public IGenericRepository<User> UserRepository { get; }
+        public IGenericRepository<Cart> CartRepository { get; }
+        public IGenericRepository<Product> ProductRepository { get; }
+        public IGenericRepository<Category> CategoryRepository { get; }
+        public IGenericRepository<Сharacteristic> СharacteristicRepository { get; }
+        public void Save();
+    }
+}
