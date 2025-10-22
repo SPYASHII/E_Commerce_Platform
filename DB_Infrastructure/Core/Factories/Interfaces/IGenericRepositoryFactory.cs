b209c3637bebfe6de6f5ece903a5a0a35c32578d
Nikita <nikitaklime2005@gmail.com>
2025-10-22 18:27:44 +0300

    ADDED: interface for GenericRepositoryFactory

---
+++ DB_Infrastructure/Core/Factories/Interfaces/IGenericRepositoryFactory.cs
@@ -0,0 +1,19 @@
+﻿using Database_Infrastructure.Core.Context.Abstract;
+using DomainComponents.Core.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database_Infrastructure.Core.Factories.Interfaces
+{
+    /// <summary>
+    /// Interface of IGenericRepositoryFactory,
+    /// in order to be able to swap concrete realizations of IGenericRepository
+    /// </summary>
+    public interface IGenericRepositoryFactory
+    {
+        public IGenericRepository<T> Create<T>(CommercePlatformContext context) where T : class;
+    }
+}
